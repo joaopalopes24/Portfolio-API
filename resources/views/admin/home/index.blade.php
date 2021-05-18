c74adcec075d756baacc1c813e859aacd005aec1
João Pedro Lopes <joaopalopes24@gmail.com>
2021-05-18 15:48:04 -0300

    Criação da Tela Inicial do Administrador

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('title', $name)
+@section('subtitle', $name)
+@section('caption', 'Graphics')
+@section('content')
+<!-- Home Content -->
+<div class="row">
+  {{ $company_name }}
+</div>
+<!-- End Content -->
+@endsection
